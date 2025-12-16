3126550885e8f3dd8092754f6f42dae11c0e8236
SalahHassan202 <salahhassan66190@gmail.com>
2025-12-16 07:33:08 +0200

    update home page file

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_calculator_app/widgets/custom_button.dart';
 
-class HomePage extends StatelessWidget {
-  final String resText = '';
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String resText = '';
 
   @override
   Widget build(BuildContext context) {
@@ -17,38 +23,82 @@ class HomePage extends StatelessWidget {
           Row(children: [Text(resText)]),
           Row(
             children: [
-              CustomButton(text: '7', onClickButton: onClickButton),
-              CustomButton(text: '8', onClickButton: onClickButton),
-              CustomButton(text: '9', onClickButton: onClickButton),
-              CustomButton(text: '*', onClickButton: onClickButton),
+              CustomButton(text: '7', onClickButton: onDigitClick),
+              CustomButton(text: '8', onClickButton: onDigitClick),
+              CustomButton(text: '9', onClickButton: onDigitClick),
+              CustomButton(text: '*', onClickButton: onOperatorClick),
             ],
           ),
           Row(
             children: [
-              CustomButton(text: '4', onClickButton: onClickButton),
-              CustomButton(text: '5', onClickButton: onClickButton),
-              CustomButton(text: '6', onClickButton: onClickButton),
-              CustomButton(text: '/', onClickButton: onClickButton),
+              CustomButton(text: '4', onClickButton: onDigitClick),
+              CustomButton(text: '5', onClickButton: onDigitClick),
+              CustomButton(text: '6', onClickButton: onDigitClick),
+              CustomButton(text: '/', onClickButton: onOperatorClick),
             ],
           ),
           Row(
             children: [
-              CustomButton(text: '1', onClickButton: onClickButton),
-              CustomButton(text: '2', onClickButton: onClickButton),
-              CustomButton(text: '3', onClickButton: onClickButton),
-              CustomButton(text: '+', onClickButton: onClickButton),
+              CustomButton(text: '1', onClickButton: onDigitClick),
+              CustomButton(text: '2', onClickButton: onDigitClick),
+              CustomButton(text: '3', onClickButton: onDigitClick),
+              CustomButton(text: '+', onClickButton: onOperatorClick),
             ],
           ),
           Row(
             children: [
-              CustomButton(text: '.', onClickButton: onClickButton),
-              CustomButton(text: '0', onClickButton: onClickButton),
-              CustomButton(text: '=', onClickButton: onClickButton),
-              CustomButton(text: '-', onClickButton: onClickButton),
+              CustomButton(text: '.', onClickButton: onDigitClick),
+              CustomButton(text: '0', onClickButton: onDigitClick),
+              CustomButton(text: '=', onClickButton: onEqual),
+              CustomButton(text: '-', onClickButton: onOperatorClick),
             ],
           ),
         ],
       ),
     );
+  }
+
+  void onDigitClick(String text) {
+    setState(() {
+      resText += text;
+    });
+  }
+
+  String num1 = '';
+  String operator = '';
+  void onOperatorClick(String onClickOperator) {
+    if (operator.isEmpty) {
+      num1 = resText;
+      operator = onClickOperator;
+      resText = '';
+    } else {
+      String num2 = resText;
+      calCulate(num1, num2, operator);
+    }
+  }
+
+  String calCulate(String num1, String num2, String operator) {
+    double number1 = double.parse(num1);
+    double number2 = double.parse(num1);
+    double res = 0.0;
+
+    if (operator == '+') {
+      res = number1 + number2;
+    } else if (operator == '-') {
+      res = number1 - number2;
+    } else if (operator == '*') {
+      res = number1 * number2;
+    } else if (operator == '/') {
+      res = number1 / number2;
+    }
+    return res.toString();
+  }
+
+  void onEqual(String onEqualOperator) {
+    String num2 = resText;
+    resText = calCulate(num1, num2, operator);
+    num1 = '';
+    operator = '';
+    setState(() {});
   }
 }
