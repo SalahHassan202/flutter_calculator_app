cff4a590569f12a0c6bd315b165d3b5f750271e5
SalahHassan202 <salahhassan66190@gmail.com>
2025-12-16 07:40:37 +0200

    finish custom widget screen

--- lib/widgets/custom_button.dart
+++ lib/widgets/custom_button.dart
@@ -2,21 +2,41 @@ import 'package:flutter/material.dart';
 
 class CustomButton extends StatelessWidget {
   final String text;
+  final Color color;
   final Function(String) onClickButton;
+
   const CustomButton({
     super.key,
     required this.text,
+    required this.color,
     required this.onClickButton,
   });
 
   @override
   Widget build(BuildContext context) {
     return Expanded(
-      child: ElevatedButton(
-        onPressed: () {
-          onClickButton(text);
-        },
-        child: Text(text, style: TextStyle(fontSize: 30)),
+      child: Padding(
+        padding: const EdgeInsets.all(6),
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: color,
+            padding: const EdgeInsets.symmetric(vertical: 22),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(12),
+            ),
+          ),
+          onPressed: () {
+            onClickButton(text);
+          },
+          child: Text(
+            text,
+            style: const TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+        ),
       ),
     );
   }
