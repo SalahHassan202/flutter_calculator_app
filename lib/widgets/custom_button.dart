c6cad890f1a194f7424e119dbc178db98d602202
SalahHassan202 <salahhassan66190@gmail.com>
2025-12-16 06:51:59 +0200

    update custom button file

--- lib/widgets/custom_button.dart
+++ lib/widgets/custom_button.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
 
 class CustomButton extends StatelessWidget {
-  const CustomButton({super.key});
+  final String text;
+  final Function onClickButton;
+  const CustomButton({
+    super.key,
+    required this.text,
+    required this.onClickButton,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Expanded(
+      child: ElevatedButton(
+        onPressed: () {},
+        child: Text(text, style: TextStyle(fontSize: 30)),
+      ),
+    );
   }
 }
